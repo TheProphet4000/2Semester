7d1a45f168917dfdaf8b0cd01635807cc239c2c6
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-05-05 11:30:31 +0200

    Update BackwardsText.java
    Har tilføjet funktionen til at kunne omvende tekst. Mangler at rette den crashende bug

--- InvertText/app/src/main/java/com/example/inverttext/BackwardsText.java
+++ InvertText/app/src/main/java/com/example/inverttext/BackwardsText.java
@@ -3,23 +3,30 @@ package com.example.inverttext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class BackwardsText extends AppCompatActivity {
 
+    TextView F = findViewById(R.id.textView); //finder text og laver en genvej til det
+
+    ArrayList<String> userInput =  getIntent().getParcelableExtra("DataToSend"); //modtager info
+    String convertedList = userInput.toString(); //laver det om til en string
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backwards_text);
 
-        ArrayList<String> userInput =  getIntent().getParcelableExtra("DataToSend");
-        String convertedList = userInput.toString();
+        reverseString(convertedList); //kalder reversString, med converted list som input
+    }
 
+    public void reverseString(String str){
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse(); //omvender tekst
 
-        TextView mainTekst = findViewById(R.id.textView);
-        mainTekst.setText(convertedList);
+        F.setText(sb.toString()); //smider det formateret text ind i view
     }
 }
